90fb55020c0cb1278a1b50758d9c5f045a8c2582
a.grakovich <drem.aimlf@gmail.com>
2017-03-07 11:28:15 +0300

    Update Header.js

--- client/components/common/Header.js
+++ client/components/common/Header.js
@@ -21,7 +21,10 @@ class Header extends React.Component {
         return (
             <header>
                 <div className="row header">
-                    <div className="col-xs-12 col-sm-6 col-md-8"><h1>Кафе желаний "Плексочка"</h1></div>
+                    <div className="col-xs-12 col-sm-6 col-md-8">
+                        <img src="http://image.prntscr.com/image/7cd28215a7f7487cbec0e313187da028.png" style="max-height: 150px;" />
+                        <h1>Кафе желаний "Плексочка"</h1>
+                    </div>
                     <div className="col-xs-6 col-md-4">
                         {this.props.logged_in &&
                         <a className="btn btn-primary pull-right logout" href="/logout" onClick={this.logOut}>Выйти</a> }
